6c00fc4f0f43a0e96a8e82f27f9e837b0ad2a7c1
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-06-18 12:34:11 +0200

    Added final touches to the CSS and fetched the JSON data

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const buttons = document.querySelectorAll("#filterButtons");
 
+const fetchData = async () => {
+  try {
+    const response = await fetch("./dat.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.forEach((entry) => console.log(entry));
+  } catch (error) {
+    console.log("Error fetching JSON:", error);
+  }
+};
+
+fetchData();
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttons.forEach((btn) => btn.classList.remove("active__button"));
