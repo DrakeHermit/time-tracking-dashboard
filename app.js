fff2bdf732f3445d9b03af706a4c4d207d722884
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-06-22 20:08:22 +0200

    Added the filtering capability

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ const buttons = document.querySelectorAll("#filterButtons");
 
 const fetchData = async () => {
   try {
-    const response = await fetch("./dat.json");
+    const response = await fetch("data.json");
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-    data.forEach((entry) => console.log(entry));
+    return data;
   } catch (error) {
     console.log("Error fetching JSON:", error);
   }
 };
 
-fetchData();
+const filterDataOnClick = (data, filter) => {
+  const filteredData = data.map(({ title, timeframes }) => ({
+    title,
+    current: timeframes[filter].current,
+    previous: timeframes[filter].previous,
+  }));
+
+  return filteredData;
+};
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", async () => {
+    const data = await fetchData();
+    const filter = button.getAttribute("data-filter");
+
+    const filteredData = filterDataOnClick(data, filter);
+    console.log(filteredData);
+
     buttons.forEach((btn) => btn.classList.remove("active__button"));
 
     button.classList.add("active__button");
